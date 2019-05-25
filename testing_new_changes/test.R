86f3091403c278f8429db227e8f0189fcca4a599
Kim Roche <kimberly.e.roche@gmail.com>
2019-05-25 16:42:42 -0400

    Calculation of collapsed form B and A and Kalman filter and simulation smoother implemented in separate CPP file; to be added to Stray

--- testing_new_changes/test.R
+++ testing_new_changes/test.R
@@ -100,34 +100,170 @@ omega <- 2*pi/5
 G <- matrix(c(cos(omega), -sin(omega), sin(omega), cos(omega)), 2, 2)
 F <- matrix(c(1, 0), 2, 1)
 W <- diag(2)
+M0 <- matrix(rnorm(10), 2, 5)
 C0 <- W
-res <- dlm_cov(T, 1, F, G, W, C0)
-res_R <- build_A(T, 1, t(F), G, W, C0) # expects transposed F!
+A <- dlm_A(T, 1, F, G, W, C0)
+A_R <- build_A(T, 1, t(F), G, W, C0) # expects transposed F!
 
-res
-res_R
+B <- dlm_B(F, G, M0, T)
 
-image(res)
-image(res_R)
+test <- rmatrixnormal(1, B, diag(5), res)[,,1]
+
+# Kalman filter
+# data_obj is a list containing ys, F, W, G, upsilon, Xi, gamma, Sigma, M.0, C.0
+# observation_vec (if present) indicates the spacing of observations, e.g. c(1, 3, 4, 7)
+#   indicates observations 2, 5, & 6 are missing and should be imputed in the usual way
+fit_filter <- function(data_obj, observation_vec=NULL) {
+  D <- ncol(data_obj$ys)
+  Theta.dim <- ncol(data_obj$G)
+  T <- nrow(data_obj$ys)
+  if(!is.null(observation_vec)) {
+    T <- max(observation_vec)
+  }
+  upsilon.t <- data_obj$upsilon
+  Xi.t <- data_obj$Xi
+  M.t <- data_obj$M.0
+  C.t <- data_obj$C.0
+  Thetas.t <- array(0, dim=c(Theta.dim, D, T)) # sample at each t
+  Cs.t <- array(0, dim=c(Theta.dim, Theta.dim, T))
+  Ms.t <- array(0, dim=c(Theta.dim, D, T))
+  Rs.t <- array(0, dim=c(Theta.dim, Theta.dim, T))
+  W.t <- NULL # last observed W.t (when using discount)
+  for(t in 1:T) {
+    R.t <- data_obj$G%*%C.t%*%t(data_obj$G) + data_obj$W
+    R.t <- round(R.t, 10)
+    Rs.t[,,t] <- R.t
+    A.t <- data_obj$G%*%M.t
+    if(!is.null(observation_vec) && !(t %in% observation_vec)) {
+      # impute
+      # carry forward without update
+      M.t <- A.t
+      Ms.t[,,t] <- M.t
+      C.t <- R.t
+      Cs.t[,,t] <- C.t
+      # no change to Sigma.t parameters (zero change in uncertainty)
+    } else {
+      f.t.T <- data_obj$F%*%A.t
+      q.t <- data_obj$gamma + (data_obj$F%*%R.t%*%t(data_obj$F))[1,1]
+      if(is.null(observation_vec)) {
+        e.t.T <- data_obj$ys[t,] - f.t.T
+      } else {
+        # there are missing observations but this is not one of them
+        # index[1]: if there are more than one observations on this date (VERY rare) just take the first
+        # need a better way of handling this
+        e.t.T <- data_obj$ys[as(which(observation_vec == t)[1], "numeric"),] - f.t.T
+      }
+      S.t <- R.t%*%t(data_obj$F)/q.t
+      M.t <- A.t + S.t%*%e.t.T
+      Ms.t[,,t] <- M.t
+      C.t <- R.t - q.t*S.t%*%t(S.t)
+      C.t <- round(C.t, 10)
+      Cs.t[,,t] <- C.t
+      upsilon.t <- upsilon.t + 1
+      Xi.t <- Xi.t + t(e.t.T)%*%e.t.T/q.t
+    }
+    Sigma.t <- rinvwishart(1, upsilon.t, Xi.t)[,,1]
+    LU <- t(chol(C.t))
+    LV <- chol(Sigma.t)
+    #Thetas.t[,,t] <- rmatrixnormal(1, M.t, C.t, Sigma.t)[,,1]
+    Thetas.t[,,t] <- M.t + LU%*%matrix(rnorm(10), 2, 5)%*%LV
+  }
+  return(list(Thetas.t=Thetas.t, upsilon=upsilon.t, Xi=Xi.t, Ms.t=Ms.t, Cs.t=Cs.t, Rs.t=Rs.t))
+}
+
+# simulation smoother
+# fit_obj is the output of fit_filter()
+fit_smoother <- function(data_obj, fit_obj) {
+  D <- ncol(data_obj$ys)
+  Theta.dim <- ncol(data_obj$G)
+  T <- dim(fit_obj$Thetas.t)[3]
+  Sigma.t <- rinvwishart(1, fit_obj$upsilon, fit_obj$Xi)[,,1]
+  Thetas.t.smoothed <- array(0, dim=c(Theta.dim, D, T))
+  Ms.t <- array(0, dim=c(Theta.dim, D, T))
+  etas.t <- matrix(0, D, T)
+  rmatnorm_mean <- fit_obj$Ms.t[,,T]
+  dim(rmatnorm_mean) <- c(Theta.dim, D) # fix if the state has one dimension only
+  # can't do drop=FALSE here
+  Thetas.t.smoothed[,,T] <- rmatrixnormal(1, rmatnorm_mean, fit_obj$Cs.t[,,T], Sigma.t)[,,1]
+  etas.t[,T] <- rmatrixnormal(1, data_obj$F%*%Thetas.t.smoothed[,,T], 1, Sigma.t)[,,1]
+  Ms.t[,,T] <- rmatnorm_mean
+  for(t in (T-1):1) {
+    Z.t <- fit_obj$Cs.t[,,t]%*%t(data_obj$G)%*%solve(fit_obj$Rs.t[,,(t+1)])
+    M.t.star <- fit_obj$Ms.t[,,t] + Z.t%*%(Thetas.t.smoothed[,,(t+1)] - data_obj$G%*%fit_obj$Ms.t[,,t])
+    Ms.t[,,t] <- M.t.star
+    C.t.star <- round(fit_obj$Cs.t[,,t] - Z.t%*%fit_obj$Rs.t[,,(t+1)]%*%t(Z.t), 10)
+    Thetas.t.smoothed[,,t] <- rmatrixnormal(1, M.t.star, C.t.star, Sigma.t)[,,1]
+    # draw an eta, this is how we'll estimate modeled covariance
+    etas.t[,t] <- rmatrixnormal(1, data_obj$F%*%Thetas.t.smoothed[,,t], 1, Sigma.t)[,,1]
+  }
+  return(list(Thetas.t=Thetas.t.smoothed, etas.t=etas.t, Ms.t=Ms.t))
+}
 
 # test random-walk ungapped Kalman filter
 
 sourceCpp("test.cpp")
 
-T <- 10
+T <- 50
 M0 <- matrix(rnorm(5), 2, 5)
 eta <- rmvnorm(T, matrix(rnorm(5), 1, 5), diag(5)) # rows are samples
 
 F <- matrix(c(1, 1), 2, 1)
 G <- diag(2)
 W <- diag(2)*0.5
-V <- diag(5)*0.5
+V <- diag(5)*0.05
 C0 <- diag(2)
 upsilon <- 7
-Xi <- diag(5)
+Xi <- diag(5)*0.1
+observation_vec <- 1:T
+observation_vec <- numeric(50)
+observation_vec[1:25] <- 1:25
+observation_vec[26:50] <- 51:75 # gap in the middle!
 
-res <- filter(eta, F, G, W, 1, upsilon, Xi, M0, C0, c(1:T))
-image(res)
+data_obj <- list(ys=eta,
+                 F=t(F),
+                 W=W,
+                 G=G,
+                 upsilon=upsilon,
+                 Xi=Xi, # center on empirical covariance
+                 gamma=1,
+                 Sigma=NULL,
+                 M.0=M0, # could randomly init too
+                 C.0=C0)
+
+
+
+# C++
+res <- filter(eta, F, G, W, 1, upsilon, Xi, M0, C0, observations=observation_vec)
+smooth <- simulation_smooth(eta, G, res$upsilon.T, res$Xi.T, res$Rs, res$Ms, res$Cs)
+
+# R
+resR <- fit_filter(data_obj, observation_vec=observation_vec)
+smoothR <- fit_smoother(data_obj, resR)
+
+
+
+T_actual <- max(observation_vec)
+
+filtered_obs1 <- numeric(T_actual)
+filtered_smooth1 <- numeric(T_actual)
+for(t in 1:T_actual) {
+  filtered_obs1[t] <- t(F)%*%res$Thetas[1:2,t]
+  filtered_smooth1[t] <- t(F)%*%smooth$Thetas[1:2,t]
+}
+plot(observation_vec, eta[,1], type="l", ylim=c(-5,5))
+lines(1:T_actual, filtered_obs1, col="blue", lty=2)
+lines(1:T_actual, filtered_smooth1, col="blue")
+
+
+filtered_obs1R <- numeric(T_actual)
+filtered_smooth1R <- numeric(T_actual)
+for(t in 1:T_actual) {
+  filtered_obs1R[t] <- t(F)%*%resR$Thetas.t[1:2,1,t]
+  filtered_smooth1R[t] <- t(F)%*%smoothR$Thetas.t[1:2,1,t]
+}
+#plot(observation_vec, eta[,1], type="l", ylim=c(-5,5))
+lines(1:T_actual, filtered_obs1R, col="red", lty=2)
+lines(1:T_actual, filtered_smooth1R, col="red")
 
 
 
--- testing_new_changes/test.cpp
+++ testing_new_changes/test.cpp
@@ -48,9 +48,22 @@ Eigen::MatrixXd power_G(Eigen::MatrixXd G,
   return reconstituted.real();
 }
 
+// [[Rcpp::export]]
+Eigen::MatrixXd dlm_B(Eigen::MatrixXd F, Eigen::MatrixXd G, Eigen::MatrixXd M0, int T) {
+  int D = M0.cols();
+  Eigen::MatrixXd B(D, T);
+  Eigen::MatrixXd alpha(1, D);
+  for(int t=1; t<=T; t++) {
+    // column-wise
+    alpha = (F.transpose())*power_G(G, t, 1)*M0;
+    B.block(0,t-1,D,1) = alpha.transpose();
+  }
+  return(B);
+}
+
 // build A matrix (covariance over samples) assuming time-invariant parameters F, G, W, gamma
 // [[Rcpp::export]]
-Eigen::MatrixXd dlm_cov(int T, double gamma, Eigen::VectorXd F, Eigen::MatrixXd G, Eigen::MatrixXd W, Eigen::MatrixXd C0) {
+Eigen::MatrixXd dlm_A(int T, double gamma, Eigen::VectorXd F, Eigen::MatrixXd G, Eigen::MatrixXd W, Eigen::MatrixXd C0) {
   // check T >= 1
   MatrixXd res = MatrixXd::Zero(T,T);
   MatrixXd Ft = F.transpose();
@@ -91,19 +104,23 @@ Eigen::MatrixXd dlm_cov(int T, double gamma, Eigen::VectorXd F, Eigen::MatrixXd 
 // in the ith row
 // again, we're assuming time-invariant F, G, W, and gamma
 // [[Rcpp::export]]
-Eigen::MatrixXd filter(Eigen::MatrixXd eta, Eigen::VectorXd F, Eigen::MatrixXd G,
+List filter(Eigen::MatrixXd eta, Eigen::MatrixXd F, Eigen::MatrixXd G,
                        Eigen::MatrixXd W, double gamma, int upsilon, Eigen::MatrixXd Xi,
                        Eigen::MatrixXd M0, Eigen::MatrixXd C0, Eigen::VectorXi observations) {
-  //List out(3);
-  //out.names() = CharacterVector::create("Thetas", "upsilon.T", "Xi.T");
+  List out(6);
+  out.names() = CharacterVector::create("Thetas", "upsilon.T", "Xi.T", "Rs", "Ms", "Cs");
   int D = eta.cols();
   int system_dim = G.cols();
-  int T = observations.maxCoeff(); // we're presuming this exists
+  int T = observations.maxCoeff();
+  // return objects
   Eigen::MatrixXd Thetas(system_dim*D, T);
+  Eigen::MatrixXd Rs(system_dim*system_dim, T);
+  Eigen::MatrixXd Ms(system_dim*D, T);
+  Eigen::MatrixXd Cs(system_dim*system_dim, T);
   // init the objects we'll iteratively overwrite where they've got an initial value
   int upsilon_t = upsilon;
   Eigen::MatrixXd Gt = G.transpose();
-  Eigen::VectorXd Ft = F.transpose();
+  Eigen::MatrixXd Ft = F.transpose();
   Eigen::MatrixXd Xi_t = Xi;
   Eigen::MatrixXd M_t = M0;
   Eigen::MatrixXd C_t = C0;
@@ -113,44 +130,131 @@ Eigen::MatrixXd filter(Eigen::MatrixXd eta, Eigen::VectorXd F, Eigen::MatrixXd G
   Eigen::MatrixXd ft_t(1, D);
   double q_t;
   Eigen::MatrixXd et_t(1, D);
-  Eigen::VectorXd S_t(system_dim);
-  Eigen::MatrixXd Sigma_t_llt(D, D);
+  Eigen::MatrixXd S_t(system_dim, 1);
+  Eigen::MatrixXd LV(D, D);
   Eigen::MatrixXd Theta_t(system_dim, system_dim);
-  for(int t=1; t<T; t++) {
+  Eigen::VectorXd res(T);
+  int curr_obs_idx = -1;
+  for(int t=1; t<=T; t++) {
+    // find the index of this time point in the observation vector (if it exists)
+    bool found = false;
+    for(int k=curr_obs_idx+1; k<observations.size(); k++) {
+      // this will skip all but the first observation on a given date
+      if(observations(k) == t) {
+        curr_obs_idx = k;
+        found = true;
+      }
+    }
+    // not found at all
+    if(!found) curr_obs_idx = -1;
     // system prior at t
     A_t = G*M_t;
     R_t = G*C_t*Gt + W;
-    // one-step ahead observation forecast
-    ft_t = Ft*A_t;
-    q_t = gamma + (Ft*R_t*F)(0,0);
-    // system posterior at t
-    et_t = eta.row(t-1) - ft_t;
-    S_t = R_t*F/q_t;
-    M_t = A_t + S_t*et_t;
-    C_t = R_t - q_t*S_t*(S_t.transpose());
+    if(curr_obs_idx < 0) {
+      M_t = A_t;
+      C_t = R_t;
+    } else {
+      // one-step ahead observation forecast
+      ft_t = Ft*A_t;
+      q_t = gamma + (Ft*R_t*F)(0,0);
+      // system posterior at t
+      et_t = eta.row(curr_obs_idx) - ft_t;
+      S_t = R_t*F/q_t;
+      M_t = A_t + S_t*et_t;
+      C_t = R_t - q_t*S_t*(S_t.transpose());
+      upsilon_t += 1;
+      Xi_t += ((et_t.transpose())*et_t)/q_t;
+    }
     Eigen::LLT<Eigen::MatrixXd> lltOfCt(C_t);
     Eigen::MatrixXd LU = lltOfCt.matrixL();
-    upsilon_t += 1;
-    Xi_t += ((et_t.transpose())*et_t)/q_t;
-    // sample Sigma.t
-    Sigma_t_llt = rInvWishRevCholesky(upsilon_t, Xi_t).matrix();
-    Theta_t = rMatNormalCholesky(M_t, LU, Sigma_t_llt);
+    // sample Sigma(t)
+    LV = rInvWishRevCholesky(upsilon_t, Xi_t).matrix().transpose(); // note this is Chol. upper triangular!
+    // sample Theta(t)
+    Theta_t = rMatNormalCholesky(M_t, LU, LV);
+    // pack up the samples to return -- R_t and C_t
     for(int i=0; i<system_dim; i++) {
       for(int j=0; j<D; j++) {
-        Thetas((i*system_dim)+j,t-1) = Theta_t(i,j);
+        Thetas(i+(j*system_dim),t-1) = Theta_t(i,j);
+        Ms(i+(j*system_dim),t-1) = M_t(i,j);
+      }
+    }
+    // pack up the samples to return -- Theta_t and M_t
+    for(int i=0; i<system_dim; i++) {
+      for(int j=0; j<system_dim; j++) {
+        Rs(i+(j*system_dim),t-1) = R_t(i,j);
+        Cs(i+(j*system_dim),t-1) = C_t(i,j);
       }
     }
   }
-  //out[0] = Thetas;
-  //out[1] = upsilon_t;
-  //out[2] = Xi_t;
-  //return(out);
-  return(Xi_t);
+  out[0] = Thetas;
+  out[1] = upsilon_t;
+  out[2] = Xi_t;
+  out[3] = Rs;
+  out[4] = Ms;
+  out[5] = Cs;
+  return(out);
 }
 
+Eigen::MatrixXd unpack_sample(Eigen::MatrixXd samples, int no_rows, int no_cols, int sample_idx) {
+  // add check for bad dimensions here
+  Eigen::MatrixXd sample(no_rows, no_cols);
+  for(int i=0; i<no_rows; i++) {
+    for(int j=0; j<no_cols; j++) {
+      sample(i,j) = samples(i+(j*no_rows),sample_idx);
+    }
+  }
+  return(sample);
+}
 
-
-
+// currently only included for diagnostic purposes
+// [[Rcpp::export]]
+List simulation_smooth(Eigen::MatrixXd eta, Eigen::MatrixXd G, int upsilon_T, Eigen::MatrixXd Xi_T,
+                       Eigen::MatrixXd Rs, Eigen::MatrixXd Ms, Eigen::MatrixXd Cs) {
+  List out(1);
+  out.names() = CharacterVector::create("Thetas");
+  int D = eta.cols();
+  int system_dim = G.cols();
+  int T = Rs.cols(); // Theta is column-collapsed here
+  Eigen::MatrixXd smoothed_Thetas(system_dim*D, T);
+  Eigen::MatrixXd LV = rInvWishRevCholesky(upsilon_T, Xi_T).matrix().transpose(); // note this is Chol. upper triangular!
+  Eigen::MatrixXd M_t = unpack_sample(Ms, system_dim, D, T-1); // M_T
+  Eigen::MatrixXd C_t = unpack_sample(Cs, system_dim, system_dim, T-1); // C_T
+  Eigen::LLT<Eigen::MatrixXd> lltOfCt(C_t);
+  Eigen::MatrixXd LU = lltOfCt.matrixL();
+  Eigen::MatrixXd smoothed_Theta_t = rMatNormalCholesky(M_t, LU, LV); // Theta_T
+  for(int i=0; i<system_dim; i++) {
+    for(int j=0; j<D; j++) {
+      smoothed_Thetas(i+(j*system_dim),T-1) = smoothed_Theta_t(i,j);
+    }
+  }
+  Eigen::MatrixXd R_t(system_dim, system_dim);
+  Eigen::MatrixXd R_t_inv(system_dim, system_dim);
+  Eigen::MatrixXd Z_t(system_dim, system_dim);
+  Eigen::MatrixXd A_t(system_dim, D);
+  Eigen::MatrixXd M_t_star(system_dim, D);
+  Eigen::MatrixXd C_t_star(system_dim, system_dim);
+  for(int t=T-1; t>0; t--) {
+    // note to self: 1-indexed loop to 0-indexed data structure
+    R_t = unpack_sample(Rs, system_dim, system_dim, t); // R_t+1
+    R_t_inv = R_t.inverse();
+    M_t = unpack_sample(Ms, system_dim, D, t-1);
+    C_t = unpack_sample(Cs, system_dim, system_dim, t-1);
+    Z_t = C_t*(G.transpose())*R_t_inv;
+    A_t = G*M_t;
+    M_t_star = M_t + Z_t*(smoothed_Theta_t - A_t);
+    C_t_star = C_t - Z_t*R_t*(Z_t.transpose());
+    lltOfCt = Eigen::LLT<Eigen::MatrixXd>(C_t_star); // reuse
+    LU = lltOfCt.matrixL();
+    smoothed_Theta_t = rMatNormalCholesky(M_t_star, LU, LV); // reuse
+    for(int i=0; i<system_dim; i++) {
+      for(int j=0; j<D; j++) {
+        smoothed_Thetas(i+(j*system_dim),t-1) = smoothed_Theta_t(i,j);
+      }
+    }
+  }
+  out[0] = smoothed_Thetas;
+  return(out);
+}
 
 
 
