66650d20637219b7e965d8976b64a3cac8265b64
Kim Roche <kimberly.e.roche@gmail.com>
2019-05-31 17:35:49 -0400

    Daily test script updates

--- testscript2.R
+++ testscript2.R
@@ -5,34 +5,34 @@ library(ggplot2)
 rm(list=ls())
 
 best_sampled <- c("DUI", "ECH", "LOG", "VET", "DUX", "LEB", "ACA", "OPH", "THR", "VAI")
-best_sampled <- c("VAI")
+best_sampled <- c("ACA")
 
-baboon <- "THR"
-full <- TRUE
-
-Q <- 2
+Q <- 3
 omega <- 2*pi/365
-G <- matrix(c(cos(omega), -sin(omega), sin(omega), cos(omega)), 2, 2)
-F <- matrix(c(1, 0), 2, 1)
-W <- diag(Q)*0.5
+G <- matrix(c(cos(omega), -sin(omega), 0, sin(omega), cos(omega), 0, 0, 0, 1), 3, 3)
+F <- matrix(c(1, 0, 1), Q, 1)
+W <- matrix(0, Q, Q)
+diag(W) <- c(0.1, 0.1, 0.1)
 C0 <- W*10
 gamma <- 1
 smooth <- TRUE
-reference_taxon <- 9 # low count group
-subset_time <- TRUE
+plot_filter <- FALSE
+subset_time <- FALSE
 n_samples <- 2000
+save_img <- TRUE
 
 for(baboon in best_sampled) {
-  # for(full in c(FALSE, TRUE)) {
   for(full in c(TRUE)) {
     cat("Fitting",baboon,"over all taxa =",full,"...\n")
     if(full) {
-      load(paste0("C:/Users/kim/Desktop/temp/",baboon,"_data.RData"))
+      load(paste0("C:/Users/kim/Documents/rules_of_life/subsetted_indiv_data/",baboon,"_data.RData"))
     } else {
-      load(paste0("C:/Users/kim/Desktop/temp/",baboon,"_subset_data.RData"))
+      load(paste0("C:/Users/kim/Documents/rules_of_life/subsetted_indiv_data/",baboon,"_subset_data.RData"))
     }
   
     Y_full <- indiv_data$ys
+    alr_Y_full <- driver::alr(Y_full + 0.5)
+    alr_means <- colMeans(alr_Y_full)
     observations_full <- indiv_data$observation_vec
   
     if(subset_time) {
@@ -56,7 +56,9 @@ for(baboon in best_sampled) {
     }
   
     D <- nrow(Y)
-    M0 <- matrix(rnorm(Q*(D-1)), Q, D-1)
+    # set Fourier coefficients uniformly at 1; set offset to mean for this logratio over all timepoints
+    M0 <- matrix(1, Q, D-1)
+    M0[3,] <- alr_means
     upsilon <- D+10
     Xi <- diag(D-1)
     
@@ -71,6 +73,7 @@ for(baboon in best_sampled) {
   
     # does a sample from Sigma look ok in scale?
     if(full) {
+      if(save_img) {
       png(paste0("C:/Users/kim/Desktop/rules_of_life_stray_run1/plots_full/",baboon,"_Sigma.png"))
       image(fit$Sigma[,,sample_no])
       dev.off()
@@ -78,6 +81,9 @@ for(baboon in best_sampled) {
       png(paste0("C:/Users/kim/Desktop/rules_of_life_stray_run1/plots_full/",baboon,"_empcov.png"))
       image(cov(driver::alr(t(Y + 0.5))))
       dev.off()
+      } else {
+        image(fit$Sigma[,,sample_no])
+      }
     }
     
     # plot the first sample of (1) log-transformed Y (2) approximated eta (3) filtered Theta (with F applied)
@@ -91,11 +97,6 @@ for(baboon in best_sampled) {
     Ft <- t(F)
     lr <- driver::alr(t(Y + 0.5))
     lr_idx <- 1
-#    if(full) {
-#      lr_idx <- 20
-#    } else {
-#      lr_idx <- 1
-#    }
     for(s in 1:n_samples) {
       ThetasF_1T <- fit$Thetas_filtered[,s]
       dim(ThetasF_1T) <- c(Q, D-1, T)
@@ -124,52 +125,50 @@ for(baboon in best_sampled) {
         }
       }
     }
-    df <- data.frame(timepoint=1:T, ymin_f=filtered_ymin, ymax_f=filtered_ymax)
-    df2 <- data.frame(timepoint=as(observations, "vector"), alrY=lr[,lr_idx])
-    df <- merge(df, df2, by='timepoint', all=TRUE)
+    df <- data.frame(timepoint=as(observations, "vector"), alrY=lr[,lr_idx])
     df2 <- data.frame(timepoint=as(observations, "vector"), eta_hat=fit$Eta[lr_idx,,sample_no])
-    df <- merge(df, df2, all=TRUE)
+    df <- merge(df, df2, by='timepoint', all=TRUE)
+    if(plot_filter) {
+      df2 <- data.frame(timepoint=1:T, ymin_f=filtered_ymin, ymax_f=filtered_ymax)
+      df <- merge(df, df2, by='timepoint', all=TRUE)
+    }
     if(smooth) {
       df2 <- data.frame(timepoint=1:T, ymin_s=smoothed_ymin, ymax_s=smoothed_ymax)
       df <- merge(df, df2, all=TRUE)
-      p <- ggplot(df, aes(timepoint)) + geom_ribbon(aes(ymin=ymin_f, ymax=ymax_f), fill = "grey90") +
-        geom_ribbon(aes(ymin=ymin_s, ymax=ymax_s), fill = "grey80") +
-        geom_point(aes(x=timepoint, y=alrY)) +
-        geom_point(aes(x=timepoint, y=eta_hat, color="blue")) +
-        theme_minimal()
-    } else {
-      p <- ggplot(df, aes(timepoint)) + geom_ribbon(aes(ymin=ymin_f, ymax=ymax_f), fill = "grey80") +
-        geom_point(aes(x=timepoint, y=alrY)) +
-        geom_point(aes(x=timepoint, y=eta_hat, color="blue")) +
-        theme_minimal()
     }
-    p
+    p <- ggplot(df, aes(timepoint))
+    if(plot_filter) {
+      p <- p + geom_ribbon(aes(ymin=ymin_f, ymax=ymax_f), fill = "grey90")
+    }
+    if(smooth) {
+      p <- p + geom_ribbon(aes(ymin=ymin_s, ymax=ymax_s), fill = "grey80")
+    }
+    p <- p + geom_point(aes(x=timepoint, y=alrY), color="red") +
+      geom_point(aes(x=timepoint, y=eta_hat), color="blue") +
+      theme_minimal()
     # FILTERING DISTRIBUTION FALLS TOWARD NEGATIVE FOR SOME LOG RATIOS, SOME INDIVIDUALS ONLY???
     # e.g. THR x 1 versus THR x 25
     # this definitely has something to do with MORE abundant vs less!!!
-    if(full) {
-      p <- p + ylab("ALR(Atopobiaceae/Helicobacteraceae)")
-    } else {
-      p <- p + ylab("ALR(Bifidobacteriaceae/Helicobacteraceae)")
-    }
-    if(full) {
-      width <- 8
-      if(subset_time) {
-        width <- 5
+    p <- p + ylab("ALR(Bifidobacteriaceae/Helicobacteraceae)")
+    if(save_img) {
+      if(full) {
+        width <- 8
+        if(subset_time) {
+          width <- 5
+        }
+        ggsave(filename=paste0("C:/Users/kim/Desktop/rules_of_life_stray_run1/plots_full/",baboon,"_Theta_smoothed.png"),
+               units="in", scale=2, width=width, height=2)
+      } else {
+        ggsave(filename=paste0("C:/Users/kim/Desktop/rules_of_life_stray_run1/plots_subset/",baboon,"_Theta_smoothed.png"),
+                               units="in", scale=2, width=5, height=2)
       }
-      ggsave(filename=paste0("C:/Users/kim/Desktop/rules_of_life_stray_run1/plots_full/",baboon,"_Theta_smoothed.png"),
-                             units="in", scale=2, width=width, height=2)
-    } else {
-      ggsave(filename=paste0("C:/Users/kim/Desktop/rules_of_life_stray_run1/plots_subset/",baboon,"_Theta_smoothed.png"),
-                             units="in", scale=2, width=5, height=2)
-    }
-  
-    if(full) {
-      sink(paste0("C:/Users/kim/Desktop/rules_of_life_stray_run1/time_breakdowns_full/",baboon,".txt"), append=FALSE, split=FALSE)
-      for(i in 1:length(fit$Timer)) {
-        cat(names(fit$Timer)[i]," -- ",round(fit$Timer[i],1),"\n")
+      if(full) {
+        sink(paste0("C:/Users/kim/Desktop/rules_of_life_stray_run1/time_breakdowns_full/",baboon,".txt"), append=FALSE, split=FALSE)
+        for(i in 1:length(fit$Timer)) {
+          cat(names(fit$Timer)[i]," -- ",round(fit$Timer[i],1),"\n")
+        }
+        sink()
       }
-      sink()
     }
   }
 }
